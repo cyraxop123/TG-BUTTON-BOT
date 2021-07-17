9ebd5a78376a3493dc7a9239a7d7ffc9f6732e62
cyraxop123 <77997274+cyraxop123@users.noreply.github.com>
2021-07-17 19:32:53 +0530

    Update sax.py

--- sax.py
+++ sax.py
@@ -1,34 +1,35 @@
-import os
-import time
-import asyncio
 import telebot
 from telebot import types
-
+import os
 
 api = os.environ.get("BOT_TOKEN")
-#api = "1853760827:AAH1twATlAeuN96f65BFtD6cqWkBDLuo8JM"
-
+sumdoo = os.environ.get("SUDO_USER").split()
+# sumdoo = "1842894003 1875503588".split()
+#api = "1725327883:AAElGwHLQnHHFPBQN7WB6KxUXd7RpF2xFns"
 bot = telebot.TeleBot(api, False)
-SUDO_USER = [1842894003, 1367789652, 1197931344]
+SUDO_USER = [1842894003,1822062027]
+sumdo = [int(i) for i in sumdoo]
 opp = []
-lop = '1. /start --> to check bot ALIVE or NOT\n2. /add <YOUR TEXT> --> To Add Poll\n3. /fuk --> Starting bot poll\n4. /clean --> To clear polls options\n5. /see --> To see avalable poll option\n6. /addsudo <USER ID> --> to give access to other user\n7. /removesudo <USER ID> -- Remove user from sudo\n8. /stopbot --> to stop the bot\n9. /sudolist --> List of sudo user\n10. /gcast <YOUR TEXT> --> brodcast the message'
-type_chat = set()
+lop = '1. /start --> to check bot ALIVE or NOT\n2. /add <YOUR TEXT> --> To Add Poll\n3. /fuk --> Starting bot poll\n4. /clean --> To clear polls options\n5. /see --> To see avalable poll option\n6. /addsudo <USER ID> --> to give access to other user\n7. /removesudo <USER ID> -- Remove user from sudo\n8. /stopbot --> to stop the bot\n9. /sudolist --> List of sudo user\n\n10.\n11. /repo --> TO SEE REPO\n\nOWNER OF BOT:  @VENOMxEzz'
+get_chat = set()
+my_dit = []
+semxy_list = set()
+
 
 @bot.message_handler(commands=['start'])
 def keybo(message):
-    type_chat.add(message.chat.id)
-    bot.reply_to(message, 'Bot Is working!\nMy Pero Master: @ArshXxD')
+    bot.reply_to(message, 'BOT IS WORKING FINE')
     bot.reply_to(message, lop)
 
 
 @bot.message_handler(commands=['fuk'])
 def hkeybo(message):
     name = message.from_user.id
-    if name in SUDO_USER:
+    if name in sumdo:
         markop = types.ReplyKeyboardMarkup()
         for i in opp:
             markop.row(i)
-        bot.send_message(message.chat.id, 'wow bhaiya kese kia', reply_markup=markop)
+        bot.send_message(message.chat.id, '@KHURANA_OP SAR HI PAMPA HAI', reply_markup=markop)
     else:
         bot.reply_to(message, 'SORRY YOU DONT HAVE ACCESS OF THIS BOT')
 
@@ -36,7 +37,7 @@ def hkeybo(message):
 @bot.message_handler(commands=['add'])
 def add_gu(message):
     name = message.from_user.id
-    if name in SUDO_USER:
+    if name in sumdo:
         txt = message.text[5:]
         bot.send_message(message.chat.id, 'DONE, WANNA ADD MORE IF YES TYPE "/add your  words"')
         try:
@@ -50,98 +51,57 @@ def add_gu(message):
 @bot.message_handler(commands=['clean'])
 def cler(message):
     name = message.from_user.id
-    if name in SUDO_USER:
+    if name in sumdo:
         opp.clear()
         bot.send_message(
             message.chat.id,
             'List is clear'
         )
     else:
-        bot.reply_to(message, 'SORRY YOU DONT HAVE ACCESS OF THIS BOT')
+        bot.reply_to(message, 'CONTACT MY OWNER FOR SUDO: @VENOMxEzz')
 
 
 @bot.message_handler(commands=['see'])
 def dekho(message):
-    for i in opp:
-        if i == None or i == 0:
-            bot.send_message('poll is empty!')
-        else:
-            bot.reply_to(
-                message,
-                f"AVAILABLE POLL OPTION: {i}"
-            )
+    name = message.from_user.id
+    if name in sumdo:
+        for i in opp:
+            if i == None or i == 0:
+                bot.send_message('poll is empty!')
+            else:
+                bot.reply_to(
+                    message,
+                    f"AVAILABLE POLL OPTION: {i}"
+                )
+    else:
+        bot.reply_to(message, 'CONTACT MY OWNER FOR SUDO: @VENOMxEzz')
 
 
 @bot.message_handler(commands=['stopbot'])
 def ending(message):
     name = message.from_user.id
-    if name in SUDO_USER:
+    if name in sumdo:
         try:
-           if message.text == '/stopbot':
-               markup = types.ReplyKeyboardRemove(selective=False)
-               bot.send_message(message.chat.id, 'thanks for using', reply_markup=markup)
+            if message.text == '/stopbot':
+                markup = types.ReplyKeyboardRemove(selective=False)
+                bot.send_message(message.chat.id, 'thanks for using', reply_markup=markup)
         except:
             pass
     else:
-        bot.reply_to(message, 'SORRY YOU DONT HAVE ACCESS OF THIS BOT')
+        bot.reply_to(message, 'CONTACT MY OWNER FOR SUDO: @VENOMxEzz')
 
 
-@bot.message_handler(commands=['addsudo'])
-def remo(message):
-    name = message.from_user.id
-    if name in SUDO_USER:
-        try:
-            sexy = int(message.text[9:])
-            SUDO_USER.append(sexy)
-            bot.send_message(message.chat.id, 'done')
-        except Exception as e:
-            bot.reply_to(message, 'please input only numbers')
-    else:
-        bot.reply_to(message, 'SORRY YOU DONT HAVE ACCESS OF THIS BOT')
-
-
-@bot.message_handler(commands=['removesudo'])
-def adem(message):
-    name = message.from_user.id
-    if name in SUDO_USER:
-        rxm = int(message.text[12:])
-        try:
-            SUDO_USER.remove(rxm)
-            bot.reply_to(message, 'remove done')
-        except Exception as e:
-            bot.reply_to(message, 'please input only numbers!')
-    else:
-        bot.reply_to(message, 'SORRY YOU DONT HAVE ACCESS OF THIS BOT')
 
 
 @bot.message_handler(commands=['sudolist'])
 def lemt(message):
     name = message.from_user.id
-    if name in SUDO_USER:
-        for i in SUDO_USER:
-            bot.reply_to(message, f"list of sudo user: {i}")
+    if name in sumdo:
+        for i in sumdo:
+            bot.send_message(message.chat.id, f"list of sudo user: {i}")
+            print(i)
 
 
-@bot.message_handler(commands=['gcast'])
-def opppp(message):
-    txt = message.text[7:]
-    name = message.from_user.id
-    if name in SUDO_USER:
-        try:
-            for i in type_chat:
-                bot.send_message(i, txt)
-                semx =+ 1
-                bot.reply_to(message, f"sending to this chat {i}")
-        except Exception as e:
-            pass
-
-@bot.message_handler(commands=['showg'])
-def pppj(message):
-    for i in type_chat:
-        bot.reply_to(message, f"BOT ACTIVE IN: {i}")
-        print(i)
-
-
-print(type_chat)
 print('nice')
+
 bot.polling(none_stop=True)
